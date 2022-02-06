846eb4a1c81d9effef5b9e01b6aaa0d343222469
zhuiguang5 <99127676+zhuiguang5@users.noreply.github.com>
2022-02-06 21:55:47 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,7 +441,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
+        if (cps <= 5) return '太菜了！';
         if (cps <= 8) return 'TCL';
         if (cps <= 10)  return 'TQL';
         if (cps <= 15) return '您';
